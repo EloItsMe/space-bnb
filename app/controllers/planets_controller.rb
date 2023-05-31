775b5483d6f3fa5879670f5475816300c605251e
Elowan Audouin-le Guyader <elowan.aud@proton.me>
2023-05-31 15:22:45 +0200

    need to finish the calendar

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,11 +1,12 @@
 class PlanetsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
-
-  def show
-    @planets = Planet.new
-  end
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @planets = Planet.all
   end
+
+  def show
+    @planet = Planet.find(params[:id])
+  end
+
 end
