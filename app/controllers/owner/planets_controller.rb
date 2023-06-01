645e90f8e21322ab7d370c2496586d3650f9319b
Thomas Dupre <thomasdpr-pro@outlook.com>
2023-06-01 17:03:59 +0200

    Form to add new planet for owner

--- app/controllers/owner/planets_controller.rb
+++ app/controllers/owner/planets_controller.rb
@@ -8,5 +8,17 @@ class Owner::PlanetsController < ApplicationController
   end
 
   def create
+    @planet = current_user.planets.new(planet_params)
+    if @planet.save
+      redirect_to owner_planets_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def planet_params
+    params.require(:planet).permit(:name, :content, :size, :galaxy, :rating, :kind, :price_per_day, :photo)
   end
 end
