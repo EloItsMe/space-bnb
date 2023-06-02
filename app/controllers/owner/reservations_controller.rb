3f59500d8b56785c37e08f44279854eeceb0507e
Thomas Dupre <thomasdpr-pro@outlook.com>
2023-06-02 06:29:19 +0200

    Update cards reservation owner pending...

--- app/controllers/owner/reservations_controller.rb
+++ app/controllers/owner/reservations_controller.rb
@@ -1,7 +1,25 @@
 module Owner
   class ReservationsController < ApplicationController
     def index
-      @reservations = Reservation.includes(:planet).where(planets: { owner: current_user }, status: 'pending')
+      @reservations = Reservation.includes(:planet).where(planets: { owner: current_user }, status: ['pending', 'accepted'])
+    end
+
+    def accept
+      reservation = Reservation.find(params[:id])
+      reservation.update(status: 'accepted')
+      redirect_to owner_reservations_path
+    end
+
+    def decline
+      reservation = Reservation.find(params[:id])
+      reservation.update(status: 'declined')
+      redirect_to owner_reservations_path
+    end
+
+    def cancel
+      reservation = Reservation.find(params[:id])
+      reservation.update(status: 'pending')
+      redirect_to owner_reservations_path
     end
   end
 end
